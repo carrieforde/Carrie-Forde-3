de60e939ad1f968fa1bd567dd7fe6923d3888c91
carrieforde <carrie@carrieforde.com>
2017-01-29 20:16:12 -0800

    Set up colors and fonts

--- functions.php
+++ functions.php
@@ -13,6 +13,22 @@ add_action( 'wp_enqueue_scripts', 'carrieforde3_enqueue_scripts', 15 );
  */
 function carrieforde3_enqueue_scripts() {
 
+	// Google fonts.
+	wp_enqueue_style(
+		'carrieforde3-google-fonts',
+		str_replace( ',', '%2C', '//fonts.googleapis.com/css?family=Assistant:300,700|Source+Code+Pro:300' ),
+		array(),
+		CARRIEFORDE3_VERSION
+	);
+
+	// Typekit font.
+	wp_enqueue_script(
+		'carrieforde3-typekit-font',
+		'//use.typekit.net/jsi5beh.js',
+		array(),
+		CARRIEFORDE3_VERSION
+	);
+
 	// Include this theme's stylesheet.
 	wp_enqueue_style(
 		'carrieforde3-style',
@@ -28,4 +44,54 @@ function carrieforde3_enqueue_scripts() {
 		array( 'jquery' ),
 		CARRIEFORDE3_VERSION
 	);
+}
+
+add_action( 'wp_head', 'carrieforde3_typekit' );
+/**
+ * Load Typekit inline JavaScript in head.
+ */
+function carrieforde3_typekit() {
+
+	echo '<script>try{Typekit.load({ async: true });}catch(e){}</script>';
+}
+
+add_filter( 'alcatraz_set_colors', 'carrieforde3_set_colors' );
+/**
+ * Set the brand colors.
+ */
+function carrieforde3_set_colors() {
+
+	$colors = array(
+		'Brand Colors'  => array(
+			'pink-cloud'   => 'linear-gradient(to top right, rgba(237, 28, 114, 0.85) 25%, rgba(234, 197, 222, 0.85))',
+			'razzmatazz'   => '#ed1c72',
+			'classic-rose' => '#eac5de',
+			'charcoal'     => '#424143',
+			'french-gray'  => '#bdbec0',
+		),
+		'Accent Colors' => array(
+			'west-side'     => '#f79811',
+			'sinopia'       => '#d63e0f',
+			'dark-violet'   => '#900ad6',
+			'blue'          => '#0d10f7',
+			'electric-lime' => '#1ced26',
+		),
+	);
+
+	return $colors;
+}
+
+add_filter( 'alcatraz_set_fonts', 'carrieforde3_set_fonts' );
+/**
+ * Set the brand fonts.
+ */
+function carrieforde3_set_fonts() {
+
+	$fonts = array(
+		'freight-display-pro' => '\'FreightDisp Pro\', serif',
+		'assistant' => '\'Assistant\', sans-serif',
+		'source-code-pro' => '\'Source Code Pro\', monospace',
+	);
+
+	return $fonts;
 }
