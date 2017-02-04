85affba2dfcb19517c1e9975c5e6a2ec7fa48928
carrieforde <carrie@carrieforde.com>
2017-02-04 10:37:40 -0800

    Update docbloc

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -2,9 +2,14 @@
 /**
  * Carrie Forde 3 custom template tags.
  *
- * @package carrie-forde3
+ * @package carrieforde3
  */
 
+/**
+ * Get the post categories and output them in a span.
+ *
+ * @author Carrie Forde
+ */
 function cf3_get_post_categories() {
 
 	$categories = get_the_category_list();
