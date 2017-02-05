8322f3a1098e304fe708bef7e15498d68f5b5c57
Carrie Forde <carrie@carrieforde.com>
2017-02-05 15:17:25 -0800

    Get styles for the portfolio card in place

--- template-parts/content-portfolio-card.php
+++ template-parts/content-portfolio-card.php
@@ -9,6 +9,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'alcatraz-col--4 portfolio-card' ); ?>>
 	<header class="entry-header portfolio-card__header">
 		<h2 class="entry-title portfolio-card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<div class="portfolio-card__services"><?php echo cf3_get_portfolio_terms(); ?></div>
 	</header>
 
 	<div class="entry-content portfolio-card__content">
