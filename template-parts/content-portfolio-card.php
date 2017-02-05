0fd8bed62f635674b094797685b3071baa652bd9
Carrie Forde <carrie@carrieforde.com>
2017-02-05 15:30:10 -0800

    Make sure the project services are clickable

--- template-parts/content-portfolio-card.php
+++ template-parts/content-portfolio-card.php
@@ -8,8 +8,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'alcatraz-col--4 portfolio-card' ); ?>>
 	<header class="entry-header portfolio-card__header">
-		<h2 class="entry-title portfolio-card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<div class="portfolio-card__services"><?php echo cf3_get_portfolio_terms(); ?></div>
+		<a href="<?php the_permalink(); ?>">
+			<h2 class="entry-title portfolio-card__title"><?php the_title(); ?></h2>
+			<div class="portfolio-card__services"><?php echo cf3_get_portfolio_terms(); ?></div>
+		</a>
 	</header>
 
 	<div class="entry-content portfolio-card__content">
