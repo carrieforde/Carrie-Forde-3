7cdd821ff56918e6a9b23315924fd77236f4458e
Carrie Forde <carrie@carrieforde.com>
2017-02-04 21:28:25 -0800

    Remove the old alcatraz card, update Thoughts card, and add Development card

--- patterns/template-parts/molecules/cards.php
+++ patterns/template-parts/molecules/cards.php
@@ -9,14 +9,7 @@
 	<h2 class="alcatraz-pattern__heading"><?php esc_html_e( 'Cards', 'alcatraz' ); ?></h2>
 
 	<?php alcatraz_pattern_doc( array(
-		'heading'      => 'Card',
-		'description'  => 'This is a card. It utilizes <code>alcatraz_image()</code> and <code>alcatraz_button</code>.',
-		'function'     => "alcatraz_card( array( 'class' => 'alcatraz-col--4' ) )",
-		'output'       => alcatraz_card( array( 'class' => 'alcatraz-col--4' ) ),
-	) ); ?>
-
-	<?php alcatraz_pattern_doc( array(
-		'heading'           => 'Post Card',
+		'heading'           => 'Post Card - Thoughts',
 		'description'       => 'This is the default card for posts.',
 		'patterns_included' => 'alcatraz_button',
 		'function'          => 'cf3_fetch_posts( array(
@@ -25,6 +18,22 @@
 ) )',
 		'output'            => cf3_fetch_posts( array(
 			'category_name' => 'thoughts',
+			'template_part' => 'template-parts/content-post-card',
+		) ),
+		'params'            => array( '$args' => 'The arguments.' ),
+		'args'              => array( 'template_part' => 'template-parts/content-post-card' ),
+	) ); ?>
+
+	<?php alcatraz_pattern_doc( array(
+		'heading'           => 'Post Card - Development',
+		'description'       => 'This is the default card for posts.',
+		'patterns_included' => 'alcatraz_button',
+		'function'          => 'cf3_fetch_posts( array(
+	\'category_name\' => \'development\',
+	\'template_part\' => \'template-parts/content-post-card\',
+) )',
+		'output'            => cf3_fetch_posts( array(
+			'category_name' => 'development',
 			'template_part' => 'template-parts/content-post-card',
 		) ),
 		'params'            => array( '$args' => 'The arguments.' ),
