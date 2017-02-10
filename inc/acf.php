26c633de848d0b05b51d3c1584dd258e392f2806
Carrie Forde <carrie@carrieforde.com>
2017-02-09 18:11:09 -0800

    Docblock & add a generic hero class

--- inc/acf.php
+++ inc/acf.php
@@ -3,6 +3,12 @@
  * ACF - get meta.
  */
 
+/**
+ * Get the ACF Hero markup.
+ *
+ * @param   int     [$post_id = 0]  The post ID.
+ * @return  string                  The hero markup.
+ */
 function cf3_get_acf_hero( $post_id = 0 ) {
 
 	if ( ! $post_id ) {
@@ -14,21 +20,20 @@ function cf3_get_acf_hero( $post_id = 0 ) {
 	$hero_description  = get_post_meta( $post_id, 'hero_description', true );
 	$image             = get_post_thumbnail_id( $post_id );
 
-
 	ob_start();
 
 	if ( ! empty( $image ) ) { ?>
 
-		<div class="hero">
+		<div class="hero hero-acf">
 			<?php echo wp_kses_post( wp_get_attachment_image( $image, 'full' ) ); ?>
 
 			<?php if ( ! empty( $hero_title ) || ! empty( $hero_description ) ) : ?>
-				<div class="hero__text">
+				<div class="hero__text hero-acf__text">
 					<?php if ( ! empty( $hero_title ) ) : ?>
-						<h2 class="hero__title"><?php echo esc_html( $hero_title ); ?></h2>
+						<h2 class="hero__title hero-acf__title"><?php echo esc_html( $hero_title ); ?></h2>
 					<?php endif; ?>
 					<?php if ( ! empty( $hero_description ) ) : ?>
-						<div class="hero__description"><?php echo esc_html( $hero_description ); ?></div>
+						<div class="hero__description hero-acf__description"><?php echo esc_html( $hero_description ); ?></div>
 					<?php endif; ?>
 				</div>
 			<?php endif; ?>
