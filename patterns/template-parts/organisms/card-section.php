0e8e6de972e791c30fff3659806d4200496c971b
Carrie Forde <carrie@carrieforde.com>
2017-02-12 11:00:48 -0800

    Update post-section to post-grid Using CSS columns, posts show in a masonry-type layout, which is documented in the Posts Grid pattern

--- patterns/template-parts/organisms/card-section.php
+++ patterns/template-parts/organisms/card-section.php
@@ -9,16 +9,16 @@
 	<h2 class="alcatraz-pattern__heading"><?php esc_html_e( 'Card Sections', 'alcatraz' ); ?></h2>
 
 	<?php alcatraz_pattern_doc( array(
-		'heading'           => 'Posts Section',
-		'description'       => 'This is the default card for posts.',
+		'heading'           => 'Posts Grid',
+		'description'       => 'This is the default posts grid layout. The posts section is wrapped in <code>' . esc_html( '<section class="posts-grid />' ) . '</code>, and utilizes a CSS-masonry effect using <code>column-count, column-gap, and break-inside</code>.',
 		'patterns_included' => 'alcatraz_button',
 		'function'          => 'cf3_fetch_posts( array(
 	\'template_part\' => \'template-parts/content-post-card\',
 ) )',
-		'output'            => cf3_fetch_posts( array(
+		'output'            => '<section class="post-grid">' . cf3_fetch_posts( array(
 			'template_part' => 'template-parts/content-post-card',
-			'posts_per_page' => 3,
-		) ),
+			'posts_per_page' => 6,
+		) ) . '</section>',
 		'params'            => array( '$args' => 'The arguments.' ),
 		'args'              => array( 'template_part' => 'template-parts/content-post-card' ),
 	) ); ?>
--- template-parts/content-post-card.php
+++ template-parts/content-post-card.php
@@ -8,32 +8,34 @@
 $accent = cf3_get_category_accent();
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'alcatraz-col--4 post-card' ); ?>>
-	<div class="post-thumbnail post-card__image">
-		<a href="<?php the_permalink(); ?>">
-			<?php if ( has_post_thumbnail() ) : ?>
-				<?php the_post_thumbnail(); ?>
-			<?php else : ?>
-				<?php echo cf3_get_category_image(); ?>
-			<?php endif; ?>
-		</a>
-	<?php echo cf3_get_post_card_category_badge(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'post-card' ); ?>>
+	<div class="post-card__inner-wrap">
+		<div class="post-thumbnail post-card__image">
+			<a href="<?php the_permalink(); ?>">
+				<?php if ( has_post_thumbnail() ) : ?>
+					<?php the_post_thumbnail(); ?>
+				<?php else : ?>
+					<?php echo cf3_get_category_image(); ?>
+				<?php endif; ?>
+			</a>
+		<?php echo cf3_get_post_card_category_badge(); ?>
+		</div>
+
+		<header class="entry-header post-card__header">
+			<h2 class="entry-title post-card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		</header>
+
+		<div class="entry-content post-card__content">
+			<?php the_excerpt(); ?>
+		</div>
+
+		<footer class="entry-footer post-card__footer">
+			<?php echo alcatraz_button( array( // WPCS: XSS OK.
+				'type' => 'text',
+				'link' => get_the_permalink(),
+				'button_text' => 'Read More',
+				'class' => 'button-text--' . esc_attr( $accent ),
+			) ); ?>
+		</footer>
 	</div>
-
-	<header class="entry-header post-card__header">
-		<h2 class="entry-title post-card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	</header>
-
-	<div class="entry-content post-card__content">
-		<?php the_excerpt(); ?>
-	</div>
-
-	<footer class="entry-footer post-card__footer">
-		<?php echo alcatraz_button( array( // WPCS: XSS OK.
-			'type' => 'text',
-			'link' => get_the_permalink(),
-			'button_text' => 'Read More',
-			'class' => 'button-text--' . esc_attr( $accent ),
-		) ); ?>
-	</footer>
 </article>
