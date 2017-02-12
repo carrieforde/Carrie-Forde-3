a92234683ebdfd0a9e991ff600957d227c4d5bd9
Carrie Forde <carrie@carrieforde.com>
2017-02-11 20:58:10 -0800

    Update fetch posts queries to allow exclusion of sticky posts

--- inc/queries.php
+++ inc/queries.php
@@ -17,13 +17,16 @@ function cf3_fetch_posts( $args = array() ) {
 		'post_type'       => 'post',
 		'posts_per_page'  => 1,
 		'template_part'   => 'template-parts/content',
+		'post__not_in'    => get_option( 'sticky_posts' ),
 	);
 	$args = wp_parse_args( $args, $defaults );
 
 	$post = array(
-		'category_name'   => esc_attr( $args['category_name'] ),
-		'post_type'       => esc_attr( $args['post_type'] ),
-		'posts_per_page'  => absint( $args['posts_per_page'] ),
+		'category_name'       => esc_attr( $args['category_name'] ),
+		'ignore_sticky_posts' => 1,
+		'post_type'           => esc_attr( $args['post_type'] ),
+		'posts_per_page'      => absint( $args['posts_per_page'] ),
+		'post__not_in'        => array_map( 'esc_attr', $args['post__not_in'] ),
 	);
 
 	$posts = new WP_Query( $post );
