2d4db226da15e513762ce60bbb4a84504da68436
Carrie Forde <carrie@carrieforde.com>
2017-02-12 15:18:07 -0800

    Add related posts to the Card Sections

--- patterns/template-parts/organisms/card-section.php
+++ patterns/template-parts/organisms/card-section.php
@@ -10,7 +10,7 @@
 
 	<?php alcatraz_pattern_doc( array(
 		'heading'           => 'Posts Grid',
-		'description'       => 'This is the default posts grid layout. The posts section is wrapped in <code>' . esc_html( '<section class="posts-grid />' ) . '</code>, and utilizes a CSS-masonry effect using <code>column-count, column-gap, and break-inside</code>.',
+		'description'       => 'This is the default posts grid layout. The posts section is wrapped in <code>' . esc_html( '<section class="posts-grid" />' ) . '</code>, and utilizes a CSS-masonry effect using <code>column-count, column-gap, and break-inside</code>.',
 		'patterns_included' => 'alcatraz_button',
 		'function'          => 'cf3_fetch_posts( array(
 	\'template_part\' => \'template-parts/content-post-card\',
@@ -25,17 +25,27 @@
 
 	<?php alcatraz_pattern_doc( array(
 		'heading'           => 'Portfolio Section',
-		'description'       => 'This is the default card for posts.',
+		'description'       => 'This is the default portfolio grid layout. The portfolio section is wrapped in <code>' . esc_html( '<section class="portfolio-grid' ) . '</code>, and this grid utilitzes CSS columns for layout.',
 		'patterns_included' => 'alcatraz_button',
 		'function'          => 'cf3_fetch_posts( array(
 	\'template_part\' => \'template-parts/content-portfolio-card\',
 ) )',
-		'output'            => cf3_fetch_posts( array(
+		'output'            => '<section class="portfolio-grid">' . cf3_fetch_posts( array(
 			'template_part' => 'template-parts/content-portfolio-card',
-			'posts_per_page' => 2,
+			'posts_per_page' => 4,
 			'post_type' => 'cf-portfolio'
-		) ),
+		) ) . '</section>',
 		'params'            => array( '$args' => 'The arguments.' ),
 		'args'              => array( 'template_part' => 'template-parts/content-post-card' ),
+	) ); ?>
+
+	<?php alcatraz_pattern_doc( array(
+		'heading'           => 'Related Posts',
+		'description'       => 'Related posts show at the bottom of the single post page. Related posts shows up to three random posts based on the single post\'s tag.',
+		'patterns_included' => '',
+		'function'          => 'cf3_get_related_posts()',
+		'output'            => cf3_get_related_posts( cf3_get_post_id() ),
+		'params'            => array(),
+		'args'              => array(),
 	) ); ?>
 </section>
