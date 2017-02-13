ded59644cdd2376da269fa69dc7a9db1392ba3d4
Carrie Forde <carrie@carrieforde.com>
2017-02-12 16:36:58 -0800

    Add inner-wrap to the portfolio card

--- template-parts/content-portfolio-card.php
+++ template-parts/content-portfolio-card.php
@@ -6,21 +6,23 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'alcatraz-col--6 portfolio-card' ); ?>>
-	<header class="entry-header portfolio-card__header">
-		<a href="<?php the_permalink(); ?>">
-			<h2 class="entry-title portfolio-card__title"><?php the_title(); ?></h2>
-			<div class="portfolio-card__services"><?php echo cf3_get_portfolio_terms(); ?></div>
-		</a>
-	</header>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'portfolio-card' ); ?>>
+	<div class="portfolio-card__inner-wrap">
+		<header class="entry-header portfolio-card__header">
+			<a href="<?php the_permalink(); ?>">
+				<h2 class="entry-title portfolio-card__title"><?php the_title(); ?></h2>
+				<div class="portfolio-card__services"><?php echo cf3_get_portfolio_terms(); ?></div>
+			</a>
+		</header>
 
-	<div class="entry-content portfolio-card__content">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="post-thumbnail portfolio-card__image">
-				<a href="<?php the_permalink(); ?>">
-					<?php the_post_thumbnail( 'card-image' ); ?>
-				</a>
-			</div>
-		<?php endif; ?>
+		<div class="entry-content portfolio-card__content">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="post-thumbnail portfolio-card__image">
+					<a href="<?php the_permalink(); ?>">
+						<?php the_post_thumbnail( 'card-image' ); ?>
+					</a>
+				</div>
+			<?php endif; ?>
+		</div>
 	</div>
 </article>
