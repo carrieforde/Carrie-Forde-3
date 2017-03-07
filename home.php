64c70208ba394f0a669532944bc03d80724c5816
Carrie Forde <carrie@carrieforde.com>
2017-03-06 22:28:47 -0800

    Adjust styles to better work with a sidebar

--- home.php
+++ home.php
@@ -23,7 +23,7 @@ get_header(); ?>
 				</header>
 			<?php endif; ?>
 
-			<div class="grid--one-two-three masonry">
+			<div class="grid--one-two masonry">
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php
