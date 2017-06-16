c4a531cdb5e1b9830a32fc7276e4f543f0948889
Carrie Forde <carrie@carrieforde.com>
2017-06-15 17:29:56 -0700

    Adds front page template

---
+++ front-page.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template for displaying all pages.
+ *
+ * @package carrieforde3
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+
+		<?php do_action( 'alcatraz_before_main' ); ?>
+
+		<main id="main" class="site-main" role="main">
+
+			<?php do_action( 'alcatraz_before_main_inside' ); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<?php
+					// Maybe load comments.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; ?>
+
+			<?php do_action( 'alcatraz_after_main_inside' ); ?>
+
+		</main>
+
+		<?php do_action( 'alcatraz_after_main' ); ?>
+
+	</div>
+<?php get_footer(); ?>
