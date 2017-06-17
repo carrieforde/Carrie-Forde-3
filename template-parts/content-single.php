1d9b2dba3edbce5a7faf022e67545bdedf5e25b4
Carrie Forde <carrie@carrieforde.com>
2017-06-16 20:46:30 -0700

    Adds speaking meta and speaking oembeds

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -4,7 +4,12 @@
  *
  * @package carrieforde3
  */
-?>
+if ( is_singular( 'cf-speaking' ) ) {
+
+	$post_id      = get_the_ID();
+	$video        = get_post_meta( $post_id, 'video_link', true );
+	$presentation = get_post_meta( $post_id, 'speaker_deck_link', true );
+} ?>
 
 <?php do_action( 'alcatraz_before_entry' ); ?>
 
@@ -13,6 +18,13 @@
 	<?php alcatraz_the_entry_header(); ?>
 
 	<div class="entry-content">
+		
+		<?php if ( is_singular( 'cf-speaking' ) ) : ?>
+			<?php echo apply_filters( 'the_content', $video ); // WPCS: XSS OK. ?>
+
+			<?php cf3_the_speaking_meta(); ?>
+		<?php endif; ?>
+
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
@@ -20,6 +32,10 @@
 				'after'  => '</div>',
 			) );
 		?>
+
+		<?php if ( is_singular( 'cf-speaking' ) ) : ?>
+			<?php echo apply_filters( 'the_content', $presentation ); ?>
+		<?php endif; ?>
 	</div>
 
 	<?php alcatraz_the_entry_footer(); ?>
