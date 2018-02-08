883b360449ae94bb03a15145886c920d0cd9898a
Carrie Forde <carrie@carrieforde.com>
2018-02-08 13:42:17 -0800

    Readds PHP post cards for taxonomy archives

---
+++ template-parts/content-post-card.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template for Post cards.
+ *
+ * @package carrieforde3
+ */
+$accent = cf3_get_category_accent();
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'card card--post alcatraz-col--4' ); ?>>
+	<div class="card__inner-wrap">
+		<div class="post-thumbnail card__image">
+			<a href="<?php the_permalink(); ?>">
+				<?php cf3_the_post_image(); ?>
+			</a>
+		<?php cf3_the_post_category_badge(); ?>
+		</div>
+
+		<header class="entry-header card__header">
+			<h2 class="entry-title card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		</header>
+
+		<div class="entry-content card__content">
+			<?php the_excerpt(); ?>
+		</div>
+
+		<footer class="entry-footer card__footer">
+			<a href="<?php the_permalink(); ?>" class="button button--text color-<?php echo esc_attr( $accent ); ?>"><?php esc_html_e( 'Read More', 'carrieforde' ); ?></a>
+		</footer>
+	</div>
+</article>
